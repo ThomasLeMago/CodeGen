3a82d7d9b1d5bf42f4fb91b8a7cfe32b602d3a26
BuildTools <unconfigured@null.spigotmc.org>
2023-01-23 20:37:47 +0100

    Started web version

---
+++ web/app.js
@@ -0,0 +1,4 @@
+const title = document.querySelector(".title");
+const btnList = document.querySelector(".genlist");
+const btnDic = document.querySelector(".gendic");
+const btnPrint = document.querySelector(".genprint")
